b31f33f25a51d0383c3663ab9bd12fabf7944a7b
Rahuls-iMac <rahwinside@gmail.com>
2020-09-05 22:46:35 +0530

    1

---
+++ Asst2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Asst2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int acc_num = int.Parse(Console.ReadLine());
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            Library lib = new Library();
+            lib.setValues(acc_num, title, author);
+            lib.display();
+            Console.WriteLine(lib.compute(days));
+        }
+    }
+
+    class Library {
+        int acc_num;
+        String title;
+        String author;
+
+        public void setValues(int acc_num, String title, String author) {
+            this.acc_num = acc_num;
+            this.title = title;
+            this.author = author;
+        }
+
+        public int compute(int days) {
+            return days * 2;
+        }
+
+        public void display() {
+            Console.WriteLine(acc_num + " " + title + " " + author);
+        }
+    }
+}
